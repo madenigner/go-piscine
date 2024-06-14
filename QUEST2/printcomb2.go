4d4ba0b7e22fdfa5e8e989a838b32143e287b3b5
DGr8est <4minedina@gmail.com>
2024-06-14 18:20:52 +0100

    Go-Piscine

---
+++ QUEST2/printcomb2.go
@@ -0,0 +1,33 @@
+package main 
+import "github.com/01-edu/z01"
+
+func main () {
+	PrintComb2 ()
+}
+func PrintComb2 () {
+	for i := '0' ; i <= '9' ; i++ {
+		for j := '0' ; j <= '9' ; j++ {
+			for k := '0' ; k <= '9' ; k++ {
+				for l := '0' ; l <= '9' ; l++ {
+					if (i*10)+j < (k*10)+l {
+						if i == '9' && j == '8' && k == '9' && l == '9' {
+							z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+						} else {
+							z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(' ')
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+					z01.PrintRune(',')
+						}
+					}
+				}
+			}
+		}
+	} 
+	z01.PrintRune('\n')
+}
---
+++ QUEST2/printnbr.go
@@ -0,0 +1,18 @@
+package main 
+import (
+	"github.com/01-edu/z01"
+	//"fmt"
+)
+
+func main () {
+	PrintNbr(0)
+}
+func PrintNbr (n int) {
+	t := '0'
+	if n == 0 {
+		z01.PrintRune(t)
+	} if n > 0 {
+		for i := 0 ; i 
+	}
+
+}
